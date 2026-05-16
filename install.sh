6da096f7e03372101191954888585b2e5d2a72ee
王志刚 <wangzhigang@wangzhigangdeMacBook-Air.local>
2026-05-16 14:03:26 +0800

    fix: use raw.githubusercontent.com/gh-pages for zip (accessible in CN)

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 echo "   version: $VERSION"
 
 # --- step 2: download ---
-ZIP_URL="https://aarontaken.github.io/sit-watcher/$APP_NAME-${VERSION_NUM}.zip"
+ZIP_URL="https://raw.githubusercontent.com/$REPO/gh-pages/$APP_NAME-${VERSION_NUM}.zip"
 TMP_DIR=$(mktemp -d)
 ZIP_PATH="$TMP_DIR/$APP_NAME.zip"
 
